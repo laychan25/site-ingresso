69bbe0cb52420451adb9efa34ff7bd2c60d50d52
laychan25 <lauriesilva0@gmail.com>
2024-10-11 15:57:17 -0300

    refatorando o codigo

--- js/app.js
+++ js/app.js
@@ -7,67 +7,68 @@ let inferior = parseInt(
 );
 let lista = document.querySelector(".lista");
 
-
-
 let botao = document.querySelector("button");
 
 function comprar() {
   let ingresso = document.getElementById("tipo-ingresso").value;
   let quantidade = document.getElementById("qtd").value;
-  //PISTA
-if(quantidade < 0  ){
-   alert('digite um numero valido')
- 
-}else{
-  if (ingresso == "pista") {
-    if (quantidade <= pista) {
-      pista = pista - quantidade;
-      lista.innerHTML = `<ul class="lista">
-         <li>Pista<span id="qtd-pista">${pista}</span></li>
-         <li>Cadeira superior <span id="qtd-superior">${superior}</span></li>
-         <li>Cadeira inferior <span id="qtd-inferior">${inferior}</span></li>
-         
-     </ul>
-     `;
-    } else if (quantidade > pista) {
-       alert('Nao há mais ingressos para a pista')
-      return;
+
+  if (quantidade < 0 ) {
+    alert("digite um numero valido");
+  } else {
+    if (ingresso == "pista") {
+      ingressoPista(quantidade);
+    }
+    if (ingresso == "superior") {
+      ingressoSuperior(quantidade);
+    }
+    if (ingresso == "inferior") {
+      ingressoInferior(quantidade);
     }
   }
-    //SUPERIOR
-  if (ingresso == "superior") {
-    
-    if (quantidade <= superior) {
-      superior = superior - quantidade;
-      lista.innerHTML = `<ul class="lista">
-         <li>Pista<span id="qtd-pista">${pista}</span></li>
-         <li>Cadeira superior <span id="qtd-superior">${superior}</span></li>
-         <li>Cadeira inferior <span id="qtd-inferior">${inferior}</span></li>
-         
-     </ul>`;
-    } else if (quantidade > superior) {
-      alert("Nao ha mais ingressos disponiveis para a cadeira superior");
-    
-      return;
-    }
-  }
-  if (ingresso == "inferior") {
-    
-    if (quantidade <= inferior) {
-      inferior = inferior - quantidade;
-      lista.innerHTML = `<ul class="lista">
-         <li>Pista<span id="qtd-pista">${pista}</span></li>
-         <li>Cadeira superior <span id="qtd-superior">${superior}</span></li>
-         <li>Cadeira inferior <span id="qtd-inferior">${inferior}</span></li>
-         
-     </ul> `;
-    } else if (quantidade > inferior) {
-      alert("Nao ha mais ingressos no inferior");
-      
-    }
-  }
-  
 }
 
+function ingressoPista(quantidade) {
+  if (quantidade <= pista) {
+    pista = pista - quantidade;
+    lista.innerHTML = `<ul class="lista">
+       <li>Pista<span id="qtd-pista">${pista}</span></li>
+       <li>Cadeira superior <span id="qtd-superior">${superior}</span></li>
+       <li>Cadeira inferior <span id="qtd-inferior">${inferior}</span></li>
+       
+   </ul>
+   `;
+  } else if (quantidade > pista) {
+    alert("Nao há mais ingressos para a pista");
+    return;
+  }
+}
 
+function ingressoSuperior(quantidade) {
+  if (quantidade <= superior) {
+    superior = superior - quantidade;
+    lista.innerHTML = `<ul class="lista">
+       <li>Pista<span id="qtd-pista">${pista}</span></li>
+       <li>Cadeira superior <span id="qtd-superior">${superior}</span></li>
+       <li>Cadeira inferior <span id="qtd-inferior">${inferior}</span></li>
+       
+   </ul>`;
+  } else if (quantidade > superior) {
+    alert("Nao ha mais ingressos disponiveis para a cadeira superior");
+    return;
+  }
+}
+
+function ingressoInferior(quantidade) {
+  if (quantidade <= inferior) {
+    inferior = inferior - quantidade;
+    lista.innerHTML = `<ul class="lista">
+       <li>Pista<span id="qtd-pista">${pista}</span></li>
+       <li>Cadeira superior <span id="qtd-superior">${superior}</span></li>
+       <li>Cadeira inferior <span id="qtd-inferior">${inferior}</span></li>
+       
+   </ul> `;
+  } else if (quantidade > inferior) {
+    alert("Nao ha mais ingressos no inferior");
+  }
 }
